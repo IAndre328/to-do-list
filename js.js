7787ac9c3f92cbf5f49ca274528cdf18b3d5cc6d
André <andrerafaelbatista@gmail.com>
2023-06-22 22:33:35 -0300

    tentando adicionar o localstorage

--- js.js
+++ js.js
@@ -1,72 +1,86 @@
-const btn_adicionar = document.querySelector("button#adicionar")
-const btn_limpar = document.querySelector("#limpar")
-const txt = document.querySelector("input#txt")
-const res = document.querySelector("#res")
+const btn_adicionar = document.querySelector("button#adicionar");
+const btn_limpar = document.querySelector("#limpar");
+const txt = document.querySelector("input#txt");
+const res = document.querySelector("#res");
+let arrayTarefas = []
 
 
 
+function tarefasCookies(){
+    console.log(localStorage.length);
 
-
-function verificar(){
-    txt.value.length > 0 ? adicionar() : alert("Insira uma tarefa a ser adicionada!")
+    if (localStorage.length > 0){
+        let arrayArmazenado = JSON.parse(localStorage.getItem("Tarefas"));
+        arrayTarefas = arrayArmazenado
+        console.log(arrayTarefas)
+        
+        Object.values(arrayTarefas).forEach(function(valor){
+            adicionar(valor)
+        }
+        )
+    }
 }
 
-function adicionar(){
-    const CoisasTarefa = document.createElement('div')
-    CoisasTarefa.classList.add('CoisasTarefa')
+function verificar(){
+    txt.value.length > 0 ? adicionar(txt.value) : alert("Insira uma tarefa a ser adicionada!");
+}
 
-    const Tarefa = document.createElement('p')
-    Tarefa.classList.add('tarefa')
-    Tarefa.innerText = txt.value
-    CoisasTarefa.appendChild(Tarefa)
+function adicionar(texto) {
+    
+    const CoisasTarefa = document.createElement('div');
+    CoisasTarefa.classList.add('CoisasTarefa');
+
+    const Tarefa = document.createElement('p');
+    Tarefa.classList.add('tarefa');
+    Tarefa.id = texto
+    Tarefa.innerText = texto;
+    CoisasTarefa.appendChild(Tarefa);
 
 
-    const excluir = document.createElement("button")
-    excluir.classList.add('excluir')
-    CoisasTarefa.appendChild(excluir)
+    const excluir = document.createElement("button");
+    excluir.classList.add('excluir');
+    CoisasTarefa.appendChild(excluir);
 
-    res.appendChild(CoisasTarefa)
-    txt.value = ""
+    res.appendChild(CoisasTarefa);
+    txt.value = "";
 
-    let listaTarefas = document.querySelectorAll(".tarefa")
-    console.log(listaTarefas)
+    arrayTarefas.push(texto)
+    localStorage.setItem("Tarefas",JSON.stringify(arrayTarefas));
+
 }
 
 function deletar(e){
     const item = e.target
-
+    const textoItem = item.parentElement.querySelector("p").textContent
+    console.log()
     if (item.classList[0] === 'excluir'){
         item.parentElement.remove()
-        console.log('hey')
+        arrayTarefas.splice(textoItem,1)
+        localStorage.setItem("Tarefas",JSON.stringify(arrayTarefas));
+
     }
    
 }
 
 function limpar(){
     res.innerHTML = ``
+    localStorage.clear()
 }
 
 txt.addEventListener("keydown",function(e){
     if (e.key == "Enter"){
-        adicionar()
+        adicionar(txt.value)
     }
-})
+});
 
-res.addEventListener("click",deletar)
-btn_limpar.addEventListener("click",limpar)
-btn_adicionar.addEventListener("click",verificar)
+res.addEventListener("click",deletar);
+btn_limpar.addEventListener("click",limpar);
+btn_adicionar.addEventListener("click",verificar);
+window.onload = tarefasCookies
 
-// armazenar o objeto
-var pessoa = {
-    nome: "André",
-    idade: "17",
-};
-localStorage.setItem("pessoa",JSON.stringify(pessoa));
 
-// recuperar o objeto
-var objetoArmazenado = localStorage.getItem("pessoa")
-objetoArmazenado = objetoArmazenado.JSON.parse(objetoArmazenado)
-console.log(objetoArmazenado)
+
+
 
 
 
